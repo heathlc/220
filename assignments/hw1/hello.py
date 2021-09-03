aec12d2aef55c659ed4e62b59a9e91287cc2f494
heathlc <heathlc@g.cofc.edu>
2021-09-02 23:11:45 -0400

    completed hw 1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,15 @@
+"""
+Name: Lydia Heath
+hello.py
+
+Problem: Printing "hello, world!"
+
+Certificate of Authenticity:
+I certifiy this project is entirely my own
+"""
+
+def main():
+    print("hello, world!")
+
+
+main()
