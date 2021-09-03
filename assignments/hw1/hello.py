796ad997b1cee647c5a483762f61454a1f8a33ad
heathlc <heathlc@g.cofc.edu>
2021-09-03 09:37:35 -0400

    completed hw 1

--- assignments/hw1/hello.py
+++ assignments/hw1/hello.py
@@ -5,8 +5,9 @@ hello.py
 Problem: Printing "hello, world!"
 
 Certificate of Authenticity:
-I certifiy this project is entirely my own
+I certify this project is entirely my own
 """
+
 
 def main():
     print("hello, world!")
